5f93dce71aa0dcc107fd14e7b1278fb06b8268ac
Harshit Singh <100908737+harshitec1011@users.noreply.github.com>
2023-11-08 23:18:01 +0530

    Create SwaggerConfig.java

---
+++ SwaggerConfig.java
@@ -0,0 +1,11 @@
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.springtutorial"))
+                .build();
+    }
